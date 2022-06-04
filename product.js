626295f9edb1131f5ac006298da298401a705716
Daniyal Memon <99191892+DaniyalMemon@users.noreply.github.com>
2022-06-04 16:14:09 +0500

    Create product.js

---
+++ product.js
@@ -0,0 +1,13 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyD2IZyjnCQEij8Tq1VWyOpLJzribHlDZo0",
+    authDomain: "dot-sneakers.firebaseapp.com",
+    projectId: "dot-sneakers",
+    storageBucket: "dot-sneakers.appspot.com",
+    messagingSenderId: "996413026257",
+    appId: "1:996413026257:web:13c4f48af881fa843dbaf0",
+    measurementId: "G-NTV0R7R5HR"
+  };
+  
+  const app = firebase.initializeApp(firebaseConfig);
+  console.log(app)
+  
